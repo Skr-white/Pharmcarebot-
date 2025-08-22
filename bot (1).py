843a18812342cd439ee5c04ffb4be709dc6223e3
Pharmcarebot <adeadebayo598@gmail.com>
2025-08-22 20:33:49 +0100

    Add files via upload

---
+++ bot (1).py
@@ -0,0 +1,21 @@
+
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+
+TOKEN ="8282174001:AAHIDJ-BBJoRJ_G1rf04O6PcF8xFKcHKvgA"
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Hello 👋! I am your PharmCare Bot. How can I help you today?")
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("You can use /start to begin chatting with me.")
+
+def main():
+    app = Application.builder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("help", help_command))
+    print("🤖 Bot is running...")
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
